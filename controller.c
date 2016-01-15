4911212c56a543fdb1664fc42cdb3f2d8ca5891a
Fan Wu <fan.wu.12@ucl.ac.uk>
2016-01-15 03:15:17 +0000

    fix rename

--- controller.c
+++ controller.c
@@ -749,7 +749,7 @@ MResult* inject_mutations(char* srcDir,char*target,char*makeDir,char* original_f
   char *mv_dir = malloc(snprintf(NULL, 0, "%s.original", tmp) + 1);
   sprintf(mv_dir, "%s.original",tmp);
   
-  int ret = rename(target, mv_dir);
+  int ret = copy_file(target, mv_dir);
   if(ret != 0)
   {
     fprintf(stderr, "Can't move %s\n", target);
